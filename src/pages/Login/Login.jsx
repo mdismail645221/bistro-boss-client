71f0f03064a0a6c3a472164c464e3430d94f67a7
Ismail Hossain <mdismail645221@gmail.com>
2023-11-18 21:56:14 +0600

    added login component. also, added the react simple captcha package

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import fetchedImgSrc from "../../assets/reservation/wood-grain-pattern-gray1x.png";
 import authLoingImg from "../../assets/others/authentication2.png";
 
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  LoadCanvasTemplateNoReload,
+  validateCaptcha,
+} from "react-simple-captcha";
+
 const Login = () => {
+  const captchaText = useRef(null);
+  const [disable, setDisable] = useState(true);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log({ email, password });
+  };
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        const form = event.target;
-        const email = form.email.value; 
-        const password = form.password.value; 
-        console.log({email, password})
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
+
+  function handleValidatedCaptcha() {
+    const user_captcha_value = captchaText.current.value;
+    if (validateCaptcha(user_captcha_value) == true) {
+      setDisable(false);
+    } else {
+      setDisable(true);
+      alert("Captcha Does Not Match");
     }
-
+  }
 
   return (
     <section
-    className="section-py"
+      className=""
       style={{
         backgroundImage: `url(${fetchedImgSrc})`,
         backgroundSize: "cover",
@@ -25,12 +46,16 @@ const Login = () => {
       }}
     >
       <div className="loginContainer container mx-auto">
-        <div>
+        <div className="flex items-center justify-center h-screen">
           <div className="sm:flex sm:flex-col md:flex md:flex-row  md:items-center md:justify-center">
-            <div className="text-center lg:text-left">
-              <img src={authLoingImg} alt="" />
+            {/* form img box */}
+            <div className="text-center lg:text-left w-1/2">
+              <img width={"100%"} src={authLoingImg} alt="" />
             </div>
-            <div className="card  shadow-2xl bg-base-100">
+            {/* form img end box */}
+
+            {/* form box */}
+            <div className="card  shadow-2xl bg-base-100 w-1/2">
               <form onSubmit={handleSubmit} className="card-body">
                 <div className="form-control">
                   <label className="label">
@@ -44,6 +69,7 @@ const Login = () => {
                     required
                   />
                 </div>
+
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Password</span>
@@ -61,14 +87,46 @@ const Login = () => {
                     </a>
                   </label>
                 </div>
+                {/* capcha load  */}
+                <div className="form-control flex">
+                  <label className="label">
+                    <LoadCanvasTemplate />
+                  </label>
+
+                  <div className="flex items-center justify-between gap-1">
+                    <input
+                      type="text"
+                      ref={captchaText}
+                      placeholder="Write Captcha text"
+                      className="input input-bordered flex-1"
+                      required
+                    />
+                    <button
+                      onClick={handleValidatedCaptcha}
+                      className="btn btn-primary"
+                    >
+                      Validate
+                    </button>
+                  </div>
+
+                </div>
+
                 <div className="form-control mt-6">
-                  <input className="btn btn-primary" type="submit" value="LOGIN" />
+                  <input
+                    className="btn btn-primary"
+                    type="submit"
+                    value="LOGIN"
+                    disabled={disable}
+                  />
                 </div>
               </form>
             </div>
+            {/* form end box  */}
           </div>
-        </div> {/* hero end tag */} 
-      </div>{/* login Container end tag */} 
+        </div>{" "}
+        {/* hero end tag */}
+      </div>
+      {/* login Container end tag */}
     </section>
   );
 };
